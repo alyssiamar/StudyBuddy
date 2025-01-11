8db4528f64520822515ecdc05a9fe104c4fbe652
angelawwu236 <zhu740513@gmail.com>
2025-01-11 13:30:32 -0800

    updated homepage

---
+++ src/components/Sidebar.js
@@ -0,0 +1,44 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import '../styles/Sidebar.css';
+
+// export default function Sidebar() {
+//   return (
+//     <div className="sidebar">
+//       <h2>Menu</h2>
+//       <ul>
+//         <li>
+//           <Link to="/music">Study Music</Link>
+//         </li>
+//         <li>
+//           <Link to="/rooms">Study Rooms</Link>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Menu</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/playlists">Study Music</Link>
+        </li>
+        <li>
+          <Link to="/rooms">Study Room</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+
